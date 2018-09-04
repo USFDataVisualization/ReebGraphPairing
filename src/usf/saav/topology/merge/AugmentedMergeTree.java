a9f0abd585df8f2b3d946cbc3a84707ab070ebfb
Paul Rosen <prosen@usf.edu>
2018-09-04 14:13:52 -0400

    added code for large augmented tree (not currently working)

--- src/usf/saav/topology/merge/AugmentedMergeTree.java
+++ src/usf/saav/topology/merge/AugmentedMergeTree.java
@@ -1,6 +1,9 @@
 package usf.saav.topology.merge;
 
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import usf.saav.topology.TopoGraph;
 
@@ -48,24 +51,71 @@ public class AugmentedMergeTree extends MergeTree {
 	
 	protected AugmentedMergeTreeNode processTree( MergeTreeNode current ){
 		
-		AugmentedMergeTreeNode ret= null;
+		/*
+		Queue<MergeTreeNode> procQueue = new LinkedList<MergeTreeNode>();
+		HashMap<MergeTreeNode,AugmentedMergeTreeNode> childMap = new HashMap<MergeTreeNode,AugmentedMergeTreeNode>();
+		
+		
+		procQueue.add(_current);
+		
+		AugmentedMergeTreeNode _head = null;
+
+		while( !procQueue.isEmpty() ) {
+			MergeTreeNode top = procQueue.peek();
+			MergeTreeNode cur = procQueue.poll();
+			while( cur.childCount() == 1 ){
+				cur = cur.getChild(0);
+			}
+			
+			if( cur.childCount() == 0 ){
+				AugmentedMergeTreeNode ret = null;
+				ret = new AugmentedMergeTreeNode( cur.getID(), cur.getValue(), cur );
+				grid.add( ret );
+				if( childMap.containsKey(top) )
+					childMap.get( top ).addChild( ret );
+			}
+
+			if( cur.childCount() == 2 ){
+				AugmentedMergeTreeNode ret = null;
+				ret = new AugmentedMergeTreeNode( cur.getID(), cur.getValue(), cur );
+				grid.add( ret );
+				if( head == null ) {
+					_head = ret;
+				}
+				if( childMap.containsKey(top) )
+					childMap.get( top ).addChild( ret );
+				
+				childMap.put( cur.getChild(0), ret );
+				childMap.put( cur.getChild(1), ret );
+				
+				procQueue.add(cur.getChild(0));
+				procQueue.add(cur.getChild(1));
+			}			
+			
+		}
+
+		return _head;
+		*/
+		
+		
+		
+		AugmentedMergeTreeNode ret = null;
 		while( current.childCount() == 1 ){
 			current = current.getChild(0);
 		}
 		if( current.childCount() == 0 ){
 			ret = new AugmentedMergeTreeNode( current.getID(), current.getValue(), current );
+			grid.add( ret );
 		}
 		if( current.childCount() == 2 ){
 			ret = new AugmentedMergeTreeNode( current.getID(), current.getValue(), current,
 							processTree( current.getChild(0) ),
 							processTree( current.getChild(1) )  );
-		}
-
-		if( ret != null )
 			grid.add( ret );
+		}
 		
 		return ret;
-
+		
 	}
 	
 	
